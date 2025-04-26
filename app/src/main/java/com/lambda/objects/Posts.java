cdf28f7dfc1f65ec8a9860b0f9722d80a01ef9f3
CodeWithHari1288 <harikrishna.1288.aws@gmail.com>
2025-04-26 12:15:08 +0530

    rest template

---
+++ app/src/main/java/com/lambda/objects/Posts.java
@@ -0,0 +1,13 @@
+package com.lambda.objects;
+
+import lombok.Data;
+
+@Data
+public class Posts {
+
+    private Integer userId;
+    private Integer id;
+    private String title;
+    private String body;
+
+}
--- app/src/main/java/com/lambda/rest/TestRepository.java
+++ app/src/main/java/com/lambda/rest/TestRepository.java
@@ -1,15 +1,34 @@
 package com.lambda.rest;
 
+import com.lambda.objects.Posts;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 @Repository
 public class TestRepository {
 
+    @Autowired
+    RestTemplate restTemplate;
+
+    @Value("${test.url}")
+    private String url;
+
     @GetMapping
     public ResponseEntity getMethod(){
-        return new ResponseEntity<>("Returning Value from REST Get",HttpStatus.ACCEPTED);
+
+        RestTemplate template = new RestTemplate();
+        ResponseEntity<Posts[]> forObject = template.getForEntity(url, Posts[].class );
+        Posts[] posts = forObject.getBody();
+        List<Posts> list = Arrays.asList(posts);
+        return new ResponseEntity<>(list,HttpStatus.ACCEPTED);
     }
 }
